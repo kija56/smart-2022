0e1eb19a6c395f3e09feaa1bc46c6636c8732ee0
kija56 <kijamabula2@gmail.com>
2022-07-17 13:31:25 +0300

    feat: add database design

---
+++ database/migrations/2022_07_16_074512_create_records_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('year');
+            $table->float('life_expectancy')->default(0);
+            $table->timestamps();
+
+            $table->foreignUuid('country_id')->nullable()->constrained('countries')->cascadeOnUpdate()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+};
